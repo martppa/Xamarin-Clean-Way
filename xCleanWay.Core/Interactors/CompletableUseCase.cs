9bdd4f1d44c15eb3c5e74bae8aaca0971f2a3989
dieppa <hm.dieppa@gmail.com>
2018-08-10 14:14:16 +0200

    Base usescases defined

---
+++ xCleanWay.Core/Interactors/CompletableUseCase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Reactive.Linq;
+using xCleanWay.Core.Threading;
+using xCleanWay.Core.Utils;
+
+namespace xCleanWay.Core.Interactors
+{
+    public abstract class CompletableUseCase<Parameters> : UseCase
+    {
+        private IExecutionThread uiThread;
+        private IExecutionThread workerThread;
+
+        public CompletableUseCase(IExecutionThread uiThread, IExecutionThread workerThread)
+        {
+            this.uiThread = uiThread;
+            this.workerThread = workerThread;
+        }
+
+        protected abstract IObservable<None> BuildUseCaseObservable(Parameters parameters);
+
+        public void Execute(Action<Exception> onError, Action onCompleted, Parameters parameters)
+        {
+            AddDisposable(BuildUseCaseObservable(parameters)
+                .SubscribeOn(workerThread.GetScheduler())
+                .ObserveOn(uiThread.GetScheduler())
+                .Subscribe(_ => { }, onError, onCompleted));
+        }
+    }
+
+}
